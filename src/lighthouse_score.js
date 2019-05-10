2b7531ac494ee0c3b1ac8781b6d13136b4c9ad2e
allenbargi <allen.bargi@volvocars.com>
2019-05-10 22:37:27 +0200

    Light house

--- src/lighthouse_score.js
+++ src/lighthouse_score.js
@@ -1,31 +1,54 @@
 const lighthouse = require("lighthouse");
 const chromeLauncher = require("chrome-launcher");
-const path = require("path");
-const fs = require("fs");
+const page = require("./page");
 
-const launchChromeAndRunLighthouse = async (url, opts, config = null) => {
-  return chromeLauncher
-    .launch({ chromeFlags: opts.chromeFlags })
-    .then(chrome => {
-      opts.port = chrome.port;
-      return lighthouse(url, opts, config).then(results => {
-        // use results.lhr for the JS-consumeable output
-        // https://github.com/GoogleChrome/lighthouse/blob/master/types/lhr.d.ts
-        // use results.report for the HTML/JSON/CSV output as a string
-        // use results.artifacts for the trace/screenshots/other specific case you need (rarer)
-        return chrome.kill().then(() => results.report);
-      });
-    })
-    .then(results => {
-      // Use results!
-      const fileName = path.join(__dirname, `x.json`);
-      fs.writeFileSync(fileName, results);
+let chrome = null;
+
+const launchChrome = async opts => {
+  chrome = await chromeLauncher.launch({ chromeFlags: opts.chromeFlags });
+};
+const killChrome = async () => {
+  await chrome.kill();
+};
+
+const getSummary = report => {
+  return {
+    lh_p: parseInt(report.categories["performance"].score * 100, 10),
+    lh_b: parseInt(report.categories["best-practices"].score * 100, 10),
+    lh_a: parseInt(report.categories["accessibility"].score * 100, 10),
+    lh_s: parseInt(report.categories["seo"].score * 100, 10)
+  };
+};
+
+const lighthouseReport = async (
+  opts = {
+    chromeFlags: ["--headless"]
+  },
+  config = null
+) => {
+  await launchChrome(opts);
+
+  let p = page.needsLightHouseScore();
+  console.log(p.url);
+
+  while (p) {
+    const results = await lighthouse(
+      p.url,
+      { ...opts, port: chrome.port },
+      config
+    );
+    const summary = getSummary(JSON.parse(results.report));
+    console.log(summary);
+
+    page.updateLightHouseScore({
+      ...summary,
+      lh_report: results.report,
+      url: p.url
     });
+    p = page.needsLightHouseScore();
+  }
+
+  await killChrome();
 };
 
-const opts = {
-  chromeFlags: ["--headless"]
-};
-
-// Usage:
-launchChromeAndRunLighthouse("https://www.volvocars.com/ar-ae", opts);
+lighthouseReport();
--- src/page.js
+++ src/page.js
@@ -16,6 +16,13 @@ const UPDATE = db.prepare(
 const DELETE_ALL = db.prepare("DELETE FROM pages");
 const DESTROY = db.prepare("DELETE FROM pages where url=?");
 
+const NEEDS_LIGHT_HOUSE_SCORE = db.prepare(
+  "SELECT * from pages WHERE lh_report IS NULL LIMIT 1"
+);
+const UPDATE_LIGHT_HOUSE_SCORE = db.prepare(
+  "UPDATE pages SET lh_p=@lh_p, lh_b=@lh_b, lh_a=@lh_a, lh_s=@lh_s, lh_report=@lh_report WHERE url=@url"
+);
+
 class Page {
   create = payload => CREATE.run(payload);
   update = payload => UPDATE.run(payload);
@@ -23,6 +30,8 @@ class Page {
   destroy = url => DESTROY.run(url);
   deleteAll = () => DELETE_ALL.run();
   findAllInMarket = market => FIND_ALL_IN_MARKET.all(market);
+  needsLightHouseScore = () => NEEDS_LIGHT_HOUSE_SCORE.get();
+  updateLightHouseScore = payload => UPDATE_LIGHT_HOUSE_SCORE.run(payload);
 }
 
 module.exports = new Page();
