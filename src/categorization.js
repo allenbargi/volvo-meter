44f9a1e699eca234e2603815a11a22649d38904a
allenbargi <allen.bargi@volvocars.com>
2019-05-15 10:20:23 +0200

    Wip

--- src/categorization.js
+++ src/categorization.js
@@ -1,38 +1,98 @@
 const page = require("./page");
-const markets = page.allMarkets().map(p => p.market);
+const orderBy = require("lodash/orderBy");
+// const markets = page.allMarkets().map(p => p.market);
 
-const PAGE_MAP = {
-  home: /^$/,
-  "why-volvo": /\/más-volvo|why-volvo/,
-  about: /\/about/,
-  pdp: /\/modelos|cars/,
-  "car-configurator": /\/car-configurator/,
-  buy: /\/buy/,
-  own: /\/own/,
-  suppport: /\/support/
+// const PAGE_MAP = {
+//   home: /^$/,
+//   "why-volvo": /\/más-volvo|why-volvo/,
+//   about: /\/about/,
+//   pdp: /\/modelos|cars/,
+//   "car-configurator": /\/car-configurator/,
+//   buy: /\/buy/,
+//   own: /\/own/,
+//   suppport: /\/support/
+// };
+
+// let unmapped = 0;
+// markets.forEach(m => {
+//   const urls = page
+//     .findAllInMarket(m)
+//     .map(p => p.url.replace(`https://www.volvocars.com/${m}`, ""));
+
+//   const mapped = urls.map(url => {
+//     const categorized = { url, category: null };
+//     Object.keys(PAGE_MAP).forEach(category => {
+//       if (url.match(PAGE_MAP[category])) {
+//         categorized.category = category;
+//       }
+//     });
+//     return categorized;
+//   });
+
+//   const notCategorized = mapped.filter(m => !m.category);
+//   // console.log(`https://www.volvocars.com/${m}`);
+//   // console.log(notCategorized);
+//   // console.log({ total: mapped.length, remaning: notCategorized.length });
+//   unmapped += notCategorized.length;
+// });
+
+// console.log(unmapped);
+
+const book = {};
+
+const categorize = type => {
+  if (book[type]) {
+    book[type] += 1;
+  } else {
+    book[type] = 1;
+  }
 };
 
-let unmapped = 0;
-markets.forEach(m => {
-  const urls = page
-    .findAllInMarket(m)
-    .map(p => p.url.replace(`https://www.volvocars.com/${m}`, ""));
+const normalize = type => {
+  switch (type) {
+    case "cars":
+    case "modelle":
+    case "modeles":
+    case "modelli":
+    case "modellen":
+    case "modele":
+    case "modelos":
+    case "modeller":
+    case "automobiļi":
+    case "autos":
+      return "cars";
+      break;
+    case "find-a-dealer":
+      return "find-a-dealer";
+      break;
+    case "about":
+      return "about";
+      break;
+    case "why-volvo":
+      return "why-volvo";
+      break;
+    case "homepage":
+      return "homepage";
+      break;
+    default:
+      return "UNKNOWN";
+      // return type
+      break;
+  }
+};
 
-  const mapped = urls.map(url => {
-    const categorized = { url, category: null };
-    Object.keys(PAGE_MAP).forEach(category => {
-      if (url.match(PAGE_MAP[category])) {
-        categorized.category = category;
-      }
-    });
-    return categorized;
-  });
-
-  const notCategorized = mapped.filter(m => !m.category);
-  // console.log(`https://www.volvocars.com/${m}`);
-  // console.log(notCategorized);
-  // console.log({ total: mapped.length, remaning: notCategorized.length });
-  unmapped += notCategorized.length;
+page.all().map(p => {
+  const [market, root, ...rest] = p.url
+    .replace("https://www.volvocars.com/", "")
+    .split("/");
+  categorize(normalize(root || "homepage"));
 });
 
-console.log(unmapped);
+console.dir(
+  orderBy(
+    Object.keys(book).map(i => ({ name: i, value: book[i] })),
+    "value",
+    "desc"
+  ),
+  { depth: null, colors: false }
+);
--- src/site-core.js
+++ src/site-core.js
@@ -10,6 +10,8 @@ const querystring = require("querystring");
 //   includeMetadata: false
 // }
 
+// editorial page template {BC702582-CCF2-46B7-A64F-BF9BB4CF64D7}
+
 class SiteCoreItemClient {
   constructor(host, username, password) {
     this.host = host;
@@ -23,26 +25,41 @@ class SiteCoreItemClient {
   }
 
   async login() {
-    // @ts-ignore
-    const res = await fetch(`${this.host}/sitecore/api/ssc/auth/login`, {
-      method: "POST",
-      body: JSON.stringify({
-        domain: "sitecore",
-        username: this.username,
-        password: this.password
-      }),
-      headers: { "Content-Type": "application/json" }
-    });
-
-    this.cookie = this.getCookie(res.headers.get("set-cookie"));
+    try {
+      // @ts-ignore
+      const res = await fetch(`${this.host}/sitecore/api/ssc/auth/login`, {
+        method: "POST",
+        body: JSON.stringify({
+          domain: "sitecore",
+          username: this.username,
+          password: this.password
+        }),
+        headers: { "Content-Type": "application/json" }
+      });
+      if (res.status === 200) {
+        this.cookie = this.getCookie(res.headers.get("set-cookie"));
+      } else {
+        console.error(res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async request(endpoint) {
-    // @ts-ignore
-    const res = await fetch(`${this.host}/sitecore/api/ssc/item${endpoint}`, {
-      headers: { cookie: this.cookie, "Content-Type": "application/json" }
-    });
-    return await res.json();
+    try {
+      // @ts-ignore
+      const res = await fetch(`${this.host}/sitecore/api/ssc/item${endpoint}`, {
+        headers: { cookie: this.cookie, "Content-Type": "application/json" }
+      });
+      if (res.status < 400) {
+        return await res.json();
+      } else {
+        console.error(res);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   async getItemById(id, options = {}) {
@@ -53,6 +70,35 @@ class SiteCoreItemClient {
     return await this.request(
       `/?path=${encodeURI(path)}&${querystring.stringify(options)}`
     );
+  }
+
+  async getItemAndAllChildrenByPath(path, options = {}) {
+    let parent = await this.request(
+      `/?path=${encodeURI(path)}&${querystring.stringify(options)}`
+    );
+    if (parent.HasChildren === "True") {
+      parent = {
+        ...parent,
+        children: await this.getItemAllChildren(parent.ItemID, options)
+      };
+    }
+    return parent;
+  }
+
+  async getItemAllChildren(id, options = {}) {
+    let items = await this.request(
+      `/${id}/children?${querystring.stringify(options)}`
+    );
+    const nestedItems = items.map(async item => {
+      if (item.HasChildren === "True") {
+        return {
+          ...item,
+          children: await this.getItemAllChildren(item.ItemID, options)
+        };
+      }
+      return item;
+    });
+    return await Promise.all(nestedItems);
   }
 
   async getItemChildren(id, options = {}) {
@@ -70,15 +116,20 @@ const run = async () => {
   const { SC_HOST, SC_USERNAME, SC_PASSWORD } = process.env;
   const sc = new SiteCoreItemClient(SC_HOST, SC_USERNAME, SC_PASSWORD);
   await sc.login();
-  const res = await sc.getItemByPath(
-    "/sitecore/content/CAAS/Content/Impressions/Content/Landing Page/components"
+  const res4 = await sc.getItemAndAllChildrenByPath(
+    "/sitecore/content/CAAS/Content/Impressions/Content/Landing Page",
+    { language: "de-de" }
   );
+  console.dir(res4, { depth: null, colors: true });
+  // const res = await sc.getItemByPath(
+  //   "/sitecore/content/CAAS/Content/Impressions/Content/Landing Page"
+  // );
+  // console.log(res);
 
-  console.log(res);
-  const res2 = await sc.getItemById("EBC11ECA-2ADC-4AB4-9BD4-C3120F58920C");
-  const res3 = await sc.getItemChildren("EBC11ECA-2ADC-4AB4-9BD4-C3120F58920C");
+  const res2 = await sc.getItemById("3416345C-FD7D-4F44-96AC-7D306E89136B");
   console.log(res2);
-  console.log(res3);
+  // const res3 = await sc.getItemChildren("EBC11ECA-2ADC-4AB4-9BD4-C3120F58920C");
+  // console.log(res3);
 };
 
 run();
