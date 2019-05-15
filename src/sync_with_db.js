d299b6d547929ec82c7de5df1593b48c91e51fb7
allenbargi <allen.bargi@volvocars.com>
2019-05-15 11:10:19 +0200

    Fetched new content

--- src/sync_with_db.js
+++ src/sync_with_db.js
@@ -1,13 +1,19 @@
 const page = require("./page");
 const pageModules = require("./page_modules");
 
+const isUpdated = (dbLastMod, currentLastMod) => {
+  return new Date(dbLastMod).getTime() < new Date(currentLastMod).getTime();
+};
+
 module.exports = function syncWithDB(results) {
   results.forEach(m => {
     m.pages.forEach(p => {
       try {
         const found = page.find(p.url);
         if (found) {
-          found.lastmod < p.lastmod &&
+          if (isUpdated(found.lastmod, p.lastmod)) {
+            console.log("Updated: ", p.url);
+
             page.update({
               market: m.marketName,
               url: p.url,
@@ -15,8 +21,10 @@ module.exports = function syncWithDB(results) {
               analyzed_at: null,
               lh_created_at: null
             });
-          pageModules.destroyModulesForUrl(p.url);
+            pageModules.destroyModulesForUrl(p.url);
+          }
         } else {
+          console.log("Added: ", p.url);
           page.create({
             market: m.marketName,
             url: p.url,
@@ -34,6 +42,7 @@ module.exports = function syncWithDB(results) {
     const allDBUrls = page.findAllInMarket(m.marketName).map(p => p.url);
     const deleted = allDBUrls.filter(x => !allCurrentUrls.includes(x));
     deleted.forEach(url => {
+      console.log("Deleted: ", url);
       page.destroy(url);
       pageModules.destroyModulesForUrl(url);
     });
